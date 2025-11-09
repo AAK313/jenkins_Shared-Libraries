6d2b141d80d2bf96525f04a278a665f23a1dc47f
GreeN <153783160+AAK313@users.noreply.github.com>
2025-11-10 01:16:04 +0200

    Update deployOnK8s.groovy

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,16 +1,9 @@
-
-def call(String projectDir, String fileName, String tokenId, String apiServerId) {
+def call(String projectDir, String fileName, String kubeconfigId) {
     stage('Deploy to Kubernetes') {
         dir(projectDir) {
-            withCredentials([
-                string(credentialsId: tokenId, variable: 'TOKEN'),
-                string(credentialsId: apiServerId, variable: 'APISERVER')
-            ]) {
+            withCredentials([file(credentialsId: kubeconfigId, variable: 'KUBECONFIG')]) {
                 sh """
-                    kubectl --token=\$TOKEN \
-                            --server=\$APISERVER \
-                            --insecure-skip-tls-verify=true \
-                            apply -f ${fileName}
+                    kubectl --kubeconfig=$KUBECONFIG apply -f ${fileName}
                 """
             }
         }
